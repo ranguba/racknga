8c1b2fadcca1ee95caf85234221a4727f9fe27b4
Kouhei Sutou <kou@clear-code.com>
2010-07-27 23:39:00 +0900

    add log middleware.

---
+++ lib/racknga/log_database.rb
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+require 'fileutils'
+
+require 'groonga'
+
+module Racknga
+  class LogDatabase
+    def initialize(database_path)
+      @database_path = database_path
+      @context = Groonga::Context.new(:encoding => :none)
+      ensure_database
+    end
+
+    def entries
+      @entries ||= @context["Entries"]
+    end
+
+    def ensure_database
+      if File.exist?(@database_path)
+        @database = Groonga::Database.open(@database_path, :context => @context)
+      else
+        create_database
+      end
+      ensure_tables
+    end
+
+    def close_database
+      @database.close
+    end
+
+    private
+    def create_responses_table
+      Groonga::Schema.define(:context => @context) do |schema|
+        schema.create_table("Tags",
+                            :type => :hash,
+                            :key_type => "ShortText") do |table|
+        end
+
+        schema.create_table("Paths",
+                            :type => :hash,
+                            :key_type => "ShortText") do |table|
+        end
+
+        schema.create_table("Entries") do |table|
+          table.time("time_stamp")
+          table.reference("tag", "Tags")
+          table.reference("path", "Paths")
+          table.float("runtime")
+          table.short_text("message", :compress => :zlib)
+        end
+
+        schema.change_table("Tags") do |table|
+          table.index("Entries.tag")
+        end
+
+        schema.change_table("Paths") do |table|
+          table.index("Entries.path")
+        end
+      end
+    end
+
+    def create_database
+      FileUtils.mkdir_p(File.dirname(@database_path))
+      @database = Groonga::Database.create(:path => @database_path,
+                                           :context => @context)
+    end
+
+    def ensure_tables
+      create_responses_table
+    end
+  end
+end
---
+++ lib/racknga/middleware/log.rb
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+require 'racknga/log_database'
+
+module Racknga
+  module Middleware
+    class Log
+      KEY_KEY = "racknga.logger"
+
+      def initialize(application, options={})
+        @application = application
+        @options = Utils.normalize_options(options || {})
+        database_path = @options[:database_path]
+        raise ArgumentError, ":database_path is missing" if database_path.nil?
+        @database = LogDatabase.new(database_path)
+        @logger = Logger.new(@database)
+      end
+
+      def call(environment)
+        environment["racknga.logger"] = @logger
+
+        start_time = Time.now
+        status, headers, body = @application.call(environment)
+        end_time = Time.now
+        request_time = end_time - start_time
+
+        length = headers["Content-Length"] || "-"
+        length = "0" if length == "0"
+        request = Rack::Request.new(environment)
+        format = "%s - %s [%s] \"%s %s %s\" %d %s %0.4f"
+        message = format % [request.ip || "-",
+                            environment["REMOTE_USER"] ||
+                            end_time.strftime("%d/%b/%Y %H:%M:%S"),
+                            request.request_method,
+                            environment["HTTP_VERSION"],
+                            status.to_s[0..3],
+                            length,
+                            request_time]
+        @logger.log("access",
+                    request.fullpath,
+                    message,
+                    :runtime => request_time)
+
+        [status, headers, body]
+      end
+
+      def ensure_database
+        @database.ensure_database
+      end
+
+      def close_database
+        @database.close_database
+      end
+
+      class Logger
+        def initialize(database)
+          @database = database
+          @entries = @database.entries
+        end
+
+        def log(tag, path, message, options={})
+          @entries.add(options.merge(:tag => tag,
+                                     :path => path,
+                                     :message => message))
+        end
+      end
+    end
+  end
+end
