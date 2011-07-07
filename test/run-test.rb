9685118883bb54a7b58335828dd1cc13dea1c889
Kouhei Sutou <kou@clear-code.com>
2011-07-07 17:13:30 +0900

    use test-unit gem.

--- test/run-test.rb
+++ test/run-test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2010-2011  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -23,24 +23,17 @@ $KCODE = "u" if RUBY_VERSION < "1.9"
 require 'pathname'
 
 base_dir = Pathname(__FILE__).dirname.parent.expand_path
-test_unit_dir = base_dir + "test-unit"
-test_unit_lib_dir = test_unit_dir + "lib"
 rroonga_dir = base_dir.parent + "rroonga"
 rroonga_ext_dir = rroonga_dir + "ext" + "groonga"
 rroonga_lib_dir = rroonga_dir + "lib"
 lib_dir = base_dir + "lib"
 test_dir = base_dir + "test"
 
-unless File.exist?(test_unit_dir)
-  test_unit_repository = "http://test-unit.rubyforge.org/svn/trunk/"
-  system("svn co #{test_unit_repository} #{test_unit_dir}") or exit(false)
-end
-
-$LOAD_PATH.unshift(test_unit_lib_dir.to_s)
-
+gem 'test-unit'
 require 'test/unit'
+require 'test/unit/notify'
 
-# ARGV.unshift("--priority-mode")
+Test::Unit::Priority.enable
 
 $LOAD_PATH.unshift(rroonga_ext_dir.to_s)
 $LOAD_PATH.unshift(rroonga_lib_dir.to_s)
