d08e40ccde4adfeb08cbd770a721c1a8d59e0262
Kouhei Sutou <kou@clear-code.com>
2011-08-06 23:58:05 +0900

    [middleware][instance-name] enabled by default.

--- lib/racknga.rb
+++ lib/racknga.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2010-2011  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -25,3 +25,4 @@ require 'racknga/middleware/deflater'
 require 'racknga/middleware/exception_notifier'
 require 'racknga/middleware/jsonp'
 require 'racknga/middleware/range'
+require "racknga/middleware/instance_name"
